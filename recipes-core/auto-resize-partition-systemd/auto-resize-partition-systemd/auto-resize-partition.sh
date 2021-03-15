e5a6dcf5c6a9bc77df4890833eec3e15681a628f
Michael Seidel <github@seidel-michael.de>
2021-03-15 12:33:46 +0100

    feat: Switch to readonly partition support

--- recipes-core/auto-resize-partition-systemd/auto-resize-partition-systemd/auto-resize-partition.sh
+++ recipes-core/auto-resize-partition-systemd/auto-resize-partition-systemd/auto-resize-partition.sh
@@ -13,4 +13,4 @@ e2fsck -f -a @DEVICE@p@PARTITION@
 resize2fs @DEVICE@p@PARTITION@
 mount @DEVICE@p@PARTITION@
 
-systemctl disable auto-resize-partition.service
+touch /data/etc/disable-auto-resize-partition
